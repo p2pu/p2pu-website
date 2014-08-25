ffde12bfaed4a7d8ffdf1968cfa2bd39248095b2
ercchy <erika@serox.si>
2014-08-25 19:06:12 +0200

    adding content

--- assets/js/main.js
+++ assets/js/main.js
@@ -17,6 +17,20 @@ var P2PU = window.P2PU || {};
 				iconUp: 'fa fa-chevron-down',
 				iconDown: 'fa fa-chevron-up'
 			});
+
+			// Smoth scrolling
+			$('a[href*=#]:not([href=#])').click(function () {
+				if (location.pathname.replace(/^\//, '') === this.pathname.replace(/^\//, '') && location.hostname === this.hostname) {
+					var target = $(this.hash);
+					target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+					if (target.length) {
+						$('html,body').animate({
+							scrollTop: target.offset().top - 0
+						}, 1000);
+						return false;
+					}
+				}
+			});
 		});
 	};
 
