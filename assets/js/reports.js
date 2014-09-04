ac30b3e54d101a2ea3a8220a5c96556b54068fd9
ercchy <erika@serox.si>
2014-09-04 23:44:24 +0200

    adding dynamical loading of reports and roundtables

---
+++ assets/js/reports.js
@@ -0,0 +1,39 @@
+/*global jQuery, window, console, can, document */
+
+
+var P2PU = window.P2PU || {};
+
+(function ($, P2PU) {
+
+	'use strict';
+
+
+	var init = function () {
+		$(function () {
+
+			$.ajax({
+				url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent("http://reports.p2pu.org/feed"),
+				dataType: 'json',
+				success: function (data) {
+					if (data.responseData.feed && data.responseData.feed.entries) {
+						$("#data").html(can.view("app-template", {reports: new can.List(data.responseData.feed.entries.reverse())}));
+						/*$.each(data.responseData.feed.entries, function (i, e) {
+							console.log(e);
+							console.log("------------------------");
+							console.log("title      : " + e.title);
+							console.log("thumbnail  : " + e.content);
+							console.log("url        : " + e.link);
+						});*/
+					}
+				},
+				error: function (){
+					$("#data").html('<h1>Oh dear, something went wrong, could you try again please? Or tell us about this <a href="http://community.p2pu.org/category/tech/support">here</a></h1>');
+				}
+			});
+		});
+	};
+
+	P2PU.Reports = {};
+	P2PU.Reports.init = init;
+
+}(jQuery, P2PU));
