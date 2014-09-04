aa1db454a647f9e749f571fe4dc25893ee7569dd
ercchy <erika@serox.si>
2014-09-05 01:44:51 +0200

    adding fetching of the blog

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -10,23 +10,39 @@ var P2PU = window.P2PU || {};
 
 	var init = function () {
 		$(function () {
+			var image,
+				re_image = /<img(.*?)>/,
+				tmp = document.createElement("div");
 
 			$.ajax({
 				url: document.location.protocol + '//ajax.googleapis.com/ajax/services/feed/load?v=1.0&num=10&callback=?&q=' + encodeURIComponent("http://info.p2pu.org/feed/"),
 				dataType: 'json',
 				success: function (data) {
 					if (data.responseData.feed && data.responseData.feed.entries) {
-						$("#data").html(can.view("app-template", {reports: new can.List(data.responseData.feed.entries.reverse())}));
-						$.each(data.responseData.feed.entries, function (i, e) {
-							console.log(e);
-							console.log("------------------------");
-							console.log("title      : " + e.title);
-							console.log("description: " + e.content);
-							console.log("url        : " + e.link);
+						var entries = data.responseData.feed.entries;
+						$.each(entries, function (i, e) {
+							image = re_image.exec(e.content);
+
+							if (image !== null && image !== undefined){
+								image = /"(.*?)"/.exec(/src="(.*?)"/.exec(image[0])[0]);
+								e.image = image[0];
+								console.log(e.image);
+							}
+
+							tmp.innerHTML = e.content;
+							e.content = tmp.textContent || tmp.innerText || "";
+
+
+							/*console.log(e);
+							 console.log("------------------------");
+							 console.log("title      : " + e.title);
+							 console.log("description: " + e.content);
+							 console.log("url        : " + e.link);*/
 						});
+						$("#data").html(can.view("app-template", {reports: new can.List(entries)}));
 					}
 				},
-				error: function (){
+				error: function () {
 					$("#data").html('<h1>Oh dear, something went wrong, could you try again please? Or tell us about this <a href="http://community.p2pu.org/category/tech/support">here</a></h1>');
 				}
 			});
