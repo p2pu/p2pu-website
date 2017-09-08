0deff7fa5b7a240d48cbbda33d8850a679367c91
Sharon <sharon.peishan.kennedy@gmail.com>
2017-09-08 14:00:58 -0400

    change search page to learning-circles

--- assets/js/team.js
+++ assets/js/team.js
@@ -2,6 +2,6 @@
   const teamName = new URL(window.location.href).searchParams.get('team');
   if (teamName) {
     $('#search-subtitle').hide();
-    $('#team-title').html('<p class="centered large">Sign up below for a learning circle organized by <span class="team-name">' + teamName + '</span></p><p class="centered small"><a href="/en/search/">Go to the global learning circles search page</a></p>')
+    $('#team-title').html('<p class="centered large">Sign up below for a learning circle organized by <span class="team-name">' + teamName + '</span></p><p class="centered small"><a href="/en/learning-circles/">Go to the global learning circles search page</a></p>')
   }
 })();
