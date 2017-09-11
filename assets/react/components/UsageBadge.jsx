930e8c33996e7b2c5a31aed068ed4d47fadb3945
Sharon <sharon.peishan.kennedy@gmail.com>
2017-09-11 19:23:43 -0400

    new course card lookin fly

--- assets/react/components/UsageBadge.jsx
+++ assets/react/components/UsageBadge.jsx
@@ -1,15 +1,21 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
+import ReactTooltip from 'react-tooltip'
 
-const UsageBadge = ({ number }) => {
+const UsageBadge = ({ number, id }) => {
   const display = number > 0
   const pluralizedText = number === 1 ? 'learning circle' : 'learning circles';
+  const icon = number === 1 ? 'done' : 'done_all';
+  const tooltipText = `Used by ${number} ${pluralizedText}`;
 
   if (display) {
     return (
-      <div className='usage-badge'>
+      <div className='usage-badge' data-tip data-for={id}>
         <div className='text'>
-          Used by <span className='big'>{number}</span> {pluralizedText}
+          <i className="material-icons">{icon}</i>{number}
+          <ReactTooltip id={id} class="p2pu-tooltip" place="bottom" effect="solid" aria-haspopup='true'>
+            <span>{tooltipText}</span>
+          </ReactTooltip>
         </div>
       </div>
     );
