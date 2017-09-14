3d455b1bb6665757c8007983f5b73a06c74e598d
Sharon <sharon.peishan.kennedy@gmail.com>
2017-09-14 14:09:59 -0400

    updated learning circle card with better time and date formatting

--- assets/react/components/LearningCircleCard.jsx
+++ assets/react/components/LearningCircleCard.jsx
@@ -9,22 +9,41 @@ const LearningCircleCard = (props) => {
 
   return (
     <div className="result-item grid-item col-md-4 col-sm-12 col-xs-12">
-      <div className="card lc-card col-md-8 offset-md-2 col-sm-8 offset-sm-2 col-xs-12" onClick={ goToUrl }>
-        <h4 className="title">{ props.name }</h4>
-        <p className="schedule">{ props.schedule }</p>
-        <p className="city-country">{ props.city }</p>
-        <div className="show-on-hover">
-          <p className="facilitator">Facilitator: { props.facilitator }</p>
-          <p className="location">Location: { props.location }</p>
-          <p className="duration">{ props.duration }</p>
+      <div className="card" onClick={ goToUrl }>
+        <div className='card-title'>
+          <h4 className="title">{ props.name }</h4>
         </div>
-      </div>
-      <div className="image-container col-xs-0 hidden-on-mobile">
-        <div className="image">
-          {
-            props.image &&
-            <img src={ props.image } alt={ props.name } />
-          }
+
+        <div className="image-container hidden-on-mobile">
+          <div className="image">
+            {
+              props.image &&
+              <img src={ props.image } alt={ props.name } />
+            }
+          </div>
+        </div>
+
+        <div className='card-body'>
+          <p className="schedule">
+            <i className="material-icons">schedule</i>
+            { props.schedule }
+          </p>
+          <p className="duration">
+            <i className="material-icons">today</i>
+            { props.duration }
+          </p>
+          <p className="city-country">
+            <i className="material-icons">place</i>
+            { props.city }
+          </p>
+          <p className="facilitator">
+            <i className="material-icons">face</i>
+            Facilitated by { props.facilitator }
+          </p>
+          <p className="location">
+            <i className="material-icons">store</i>
+            Meeting at { props.location }
+          </p>
         </div>
       </div>
     </div>
