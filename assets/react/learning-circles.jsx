ea6cf52ac2dd49b59fae401ed7a2e4754c5dc384
Sharon <sharon.peishan.kennedy@gmail.com>
2018-09-20 12:52:24 -0400

    use polyfilled URL instead of native

---
+++ assets/react/learning-circles.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Subtitle from './components/LearningCircles/Subtitle'
+
+ReactDOM.render(
+  <Subtitle />, document.getElementById("search-subtitle")
+);
+
