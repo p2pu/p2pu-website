04034ea9fe3e0cf7346909ed04edfe2dacde0fc8
Sharon Kennedy <sharon@nomadiclabs.ca>
2019-08-16 13:03:25 -0400

    split up testimonials on home page

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,71 +1,66 @@
+
 (function($) {
 
   // Initialize slick on testimonial slides
-  $('#testimonials .slide-container').slick({
-    centerMode: true,
-    variableWidth: true,
-    dots: true,
-    arrows: false,
-    autoplay: true,
-    autoplaySpeed: 4000,
-    pauseOnHover: true,
-    swipe: true,
-    focusOnSelect: true,
-    infinite: true,
-    slidesToShow: 1,
-    slidesToScroll: 1,
-    responsive: [
-      {
-        breakpoint: 480,
-        settings: {
-          pauseOnHover: false
-        }
-      },
-    ]
-  })
 
+  $('.p2pu-slide-container').each(function() {
 
-  // Initialize SmoothScroll
+    var slider = $(this);
 
-   new SmoothScroll('a[href*="#"]');
-
-
-  // Initialize additional scroll-based animations with ScrollMagic
-
-  function expandImageBgLines(event) {
-    var delay = 100;
-    var lines = event.currentTarget.triggerElement();
-    $(lines).children('.bg-line').each(function(index) {
-      var el = this;
-      window.setTimeout(function() {
-        $(el).addClass('expand');
-      }, delay * (index + 1))
-    });
-  }
-
-  // SmoothScroll
-
-  var controller = new ScrollMagic.Controller();
-  var learningCirclesDefinition = document.getElementById('definition');
-  var bgImageLines = document.querySelectorAll('main > .image-container .lines');
-
-  new ScrollMagic.Scene({
-    triggerElement: bgImageLines
-  })
-  .on('enter', expandImageBgLines)
-  .addTo(controller);
-
-  // Start landing page line animation on page load
-
-  $(window).on("load", function() {
-    var delay = 100;
-    var lines = $('#landing .lines')
-    $(lines).children('.bg-line').each(function(index) {
-      var el = this;
-      window.setTimeout(function() {
-        $(el).addClass('expand');
-      }, delay * (index + 1))
-    });
+    slider.slick({
+      arrows: false,
+      autoplay: true,
+      autoplaySpeed: 4000,
+      pauseOnHover: true,
+      swipe: true,
+      focusOnSelect: true,
+      infinite: true,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+      responsive: [
+        {
+          breakpoint: 480,
+          settings: {
+            pauseOnHover: false
+          }
+        },
+      ]
+    })
   })
 
 })(jQuery)
+
+
+// Initialize SmoothScroll
+new SmoothScroll('a[href*="#"]');
+
+
+// Initialize AOS
+AOS.init();
+
+
+// start video when modal opens, pause video when modal closes
+
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+
+var ytPlayer;
+function onYouTubeIframeAPIReady() {
+  ytPlayer = new YT.Player('youtube-player', {
+    height: '100%',
+    width: '100%',
+    videoId: 'bQqmIS7WQa8',
+    events: {}
+  });
+}
+
+$('#video-modal').on('shown.bs.modal', function() {
+  ytPlayer.playVideo();
+})
+
+$('#video-modal').on('hidden.bs.modal', function() {
+  ytPlayer.pauseVideo();
+})
