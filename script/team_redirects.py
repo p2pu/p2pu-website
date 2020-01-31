b621992601304b12b74c9038d78573695caa7043
Dirk Uys <dirkcuys@gmail.com>
2020-01-31 13:00:55 +0200

    Manually set berlin team redirect

--- script/team_redirects.py
+++ script/team_redirects.py
@@ -1,6 +1,6 @@
 import sys, json
 teams = json.load(sys.stdin)['items']
 
-for id, name, slug in [ (d['id'], d['name'], d['page_slug']) for d in teams if d['id'] != 3]: 
+for id, name, slug in [ (d['id'], d['name'], d['page_slug']) for d in teams if d['id'] != 3 and d['page_slug'] != 'berlin']: 
     #print(f'  - condition:\n      key_prefix_equals: {slug}\n    redirect:\n      host_name: www.p2pu.org\n      replace_key_with: en/learning-circles/?team_id={id}&team={name}\n      http_redirect_code: 302')
     print('  - condition:\n      key_prefix_equals: {}\n    redirect:\n      host_name: www.p2pu.org\n      replace_key_with: en/learning-circles/?team_id={}&team={}\n      http_redirect_code: 302'.format(slug, id, name))
