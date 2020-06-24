3135410cb9c288e2344e3e4136852e460d6811b1
Sharon Kennedy <sharon@nomadiclabs.ca>
2020-06-24 14:39:06 -0400

    remove old team page functionality

--- assets/react/search.jsx
+++ assets/react/search.jsx
@@ -7,14 +7,8 @@ import 'p2pu-search-cards/dist/build.css';
 
 let elem = document.getElementById('search-lc-component');
 
-let initialState = {}
-if (elem.dataset.teamId) {
-  initialState["team_id"] = elem.dataset.teamId
-}
-
 ReactDOM.render(
   <Search
-    initialState={initialState}
     searchSubject={'learningCircles'}
     Browse={BrowseLearningCircles}
   />, document.getElementById('search-lc-component')
