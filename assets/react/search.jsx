c61b9052b0e809cd647023d73a21a3d6d358f82b
Sharon Kennedy <sharon@nomadiclabs.ca>
2020-07-03 15:33:36 -0400

    put back initialState on search component, fix logo style

--- assets/react/search.jsx
+++ assets/react/search.jsx
@@ -6,9 +6,14 @@ import { Search, BrowseLearningCircles } from 'p2pu-search-cards';
 import 'p2pu-search-cards/dist/build.css';
 
 let elem = document.getElementById('search-lc-component');
+let initialState = {}
+if (elem.dataset.teamId) {
+  initialState["team_id"] = elem.dataset.teamId
+}
 
 ReactDOM.render(
   <Search
+    initialState={initialState}
     searchSubject={'learningCircles'}
     Browse={BrowseLearningCircles}
   />, document.getElementById('search-lc-component')
