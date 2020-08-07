f7f37c867e440ea16a74e720e0564eef88b89e04
Sharon Kennedy <sharon@nomadiclabs.ca>
2020-08-07 09:03:39 -0400

    show past learning circles

--- assets/react/search.jsx
+++ assets/react/search.jsx
@@ -15,6 +15,8 @@ ReactDOM.render(
     initialState={initialState}
     searchSubject={'learningCircles'}
     Browse={BrowseLearningCircles}
+    origin={'http://localhost:8000'}
+    contact={ elem.dataset.teamContact ? elem.dataset.teamContact : null }
   />, document.getElementById('search-lc-component')
 );
 
