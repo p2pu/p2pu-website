cb8cde08bd8b43777b49267f17b0963ba54843d7
Dirk Uys <dirkcuys@gmail.com>
2020-09-09 22:50:41 +0200

    fff

--- terraform.tf
+++ terraform.tf
@@ -21,7 +21,7 @@ resource "aws_s3_bucket" "website" {
 }
 
 output "site_url" {
-  value = aws_s3_bucket.website.website_domain
+  value = aws_s3_bucket.website.website_endpoint
 }
 
 output "bucket" {
