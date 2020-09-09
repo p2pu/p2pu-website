5c2ed7e356d86df18b26cb246cb4dcf924fc4105
Dirk Uys <dirkcuys@gmail.com>
2020-09-09 22:31:28 +0200

    Change s3 URL to website URL

--- terraform.tf
+++ terraform.tf
@@ -21,7 +21,7 @@ resource "aws_s3_bucket" "website" {
 }
 
 output "site_url" {
-  value = aws_s3_bucket.website.bucket_domain_name
+  value = aws_s3_bucket.website.website_domain
 }
 
 output "bucket" {
