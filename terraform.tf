f5284a56062e7baa5034ee83c839cc5d709b7911
Dirk Uys <dirkcuys@gmail.com>
2020-09-09 13:56:55 +0200

    Github action for terraform

---
+++ terraform.tf
@@ -0,0 +1,29 @@
+variable "branch_name" {
+  type = string
+}
+
+terraform {
+  backend "s3" {
+    bucket = "sysadmin"
+    region = "us-east-1"
+  }
+}
+
+provider "aws" {
+    region  = "us-east-1"
+}
+
+resource "random_pet" "bucket" {}
+
+resource "aws_s3_bucket" "website" {
+  bucket = "p2pu-website-${random_pet.bucket.id}"
+  acl    = "public-read"
+
+  website {
+    index_document = "index.html"
+  }
+}
+
+output "site_url" {
+  value = aws_s3_bucket.website.bucket_domain_name
+}
