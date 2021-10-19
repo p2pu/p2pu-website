c5ec762f7a81a23e442fdd85a4ac0d0926e17ca0
Dirk Uys <dirkcuys@gmail.com>
2021-10-19 16:53:55 +0200

    Add dropdown indicators

--- assets/react/components/UserDropdown.jsx
+++ assets/react/components/UserDropdown.jsx
@@ -6,6 +6,7 @@ const UserDropdown = ({user, links, apiOrigin}) => {
       <button type="button" className="btn btn-sm d-flex align-items-center secondary p2pu-btn gray mx-0 dropdown-toggle" id="loggedInDropdown" data-bs-toggle="dropdown" aria-expanded="false">
         <span className="material-icons material-icons-outlined me-1">account_circle</span>
         {user}
+        <span className="material-icons">expand_more</span>
       </button>
       <ul className="dropdown-menu py-0 dropdown-menu-end loggedin-dropdown shadow" aria-labelledby="loggedInDropdown">
         {
