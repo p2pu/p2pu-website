af41f198890c838f6337c7465247e79449b3634f
Dirk Uys <dirkcuys@gmail.com>
2021-11-11 15:18:32 +0200

    Change links in contact form confirmation/error to buttons

--- assets/react/components/Contact.jsx
+++ assets/react/components/Contact.jsx
@@ -11,7 +11,6 @@ const Contact = props => {
   const [validationErrors, setValidationErrors] = useState(null);
 
   const onSubmit = formData => {
-    console.log(formData);
     setLoading(true);
     setError(false);
     setValidationErrors(null);
@@ -38,17 +37,26 @@ const Contact = props => {
           setError(true);
         }
       }
+    }).catch(error => {
+      setLoading(false);
+      setError(true);
     });
   }
 
   return (
     <>
-      { loading && <div className="spinner">sending</div> }
+      { loading && <div className="spinner-border" role="status"><span className="visually-hidden">Loading...</span></div> }
       { result.status == 'sent' &&
-        <p>Your message has been sent. <a onClick={() => setResult({})} >Send another</a></p>
+        <>
+          <p>Your message has been sent.</p>
+          <a className="btn p2pu-btn blue" onClick={() => setResult({})} >Send another</a>
+        </>
       }
       { error && !validationErrors &&
-        <p>Something went wrong. <a onClick={() => {setResult({}); setError(false)}} >Try again</a></p>
+        <>
+          <p>Something went wrong.</p>
+          <a className="btn p2pu-btn blue" onClick={() => {setResult({}); setError(false)}} >Try again</a>
+        </>
       }
       { !loading && (!error || validationErrors) && result.status != 'sent' &&
         <ContactForm {...props} onSubmit={onSubmit} validationErrors={validationErrors} />
