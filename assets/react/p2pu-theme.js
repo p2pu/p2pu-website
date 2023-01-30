3e42b029cfdfb0ca56135395223619134f3b365e
Dirk Uys <dirkcuys@gmail.com>
2023-01-30 16:40:27 +0200

    Use material icons and fontawesome from npm packages

--- assets/react/p2pu-theme.js
+++ assets/react/p2pu-theme.js
@@ -1,2 +1,7 @@
 //import "p2pu-theme/src/js/base.js";
 import "p2pu-theme/src/js/accordion.js";
+
+import 'material-icons/iconfont/filled.scss';
+import '@fortawesome/fontawesome-free/css/brands';
+import '@fortawesome/fontawesome-free/css/solid';
+import '@fortawesome/fontawesome-free/css/fontawesome';
