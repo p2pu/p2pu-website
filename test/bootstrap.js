6d229703fe90e83c2722f24d698cff10c7cb1937
Dirk Uys <dirkcuys@gmail.com>
2023-02-22 14:01:20 +0200

    Update docker image used from running puppeteer

--- test/bootstrap.js
+++ test/bootstrap.js
@@ -1,10 +1,11 @@
-const puppeteer = require('puppeteer');
+const puppeteer = require('puppeteer-core');
 const { expect } = require('chai');
 let { browser, expect: expect_ } = global;
 const globalVariables = Object.assign({}, { browser: browser, expect: expect_ });
 
 // puppeteer options
 const opts = {
+  executablePath: '/usr/bin/google-chrome',
   timeout: 10000,
   headless: true,
   args: ['--no-sandbox', '--disable-setuid-sandbox']
