2ec17d2917692bd1daed1928ee0c1557014974ba
Dirk Uys <dirkcuys@gmail.com>
2024-03-26 13:21:30 +0200

    Add config to remove the block AWS puts on public ACL for S3 buckets

--- terraform.tf
+++ terraform.tf
@@ -16,6 +16,14 @@ resource "aws_s3_bucket" "website_bucket" {
   force_destroy = true
 }
 
+resource "aws_s3_bucket_public_access_block" "public_access_block" {
+  bucket = "p2pu-website-${random_pet.bucket.id}"
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "aws_s3_bucket_website_configuration" "website" {
   bucket = "p2pu-website-${random_pet.bucket.id}"
 
